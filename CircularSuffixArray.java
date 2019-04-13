c5324452d66844da17a11ba64c05d8d2f91229c1
mgruben <GrubenM@gmail.com>
2019-04-13 10:07:26 -0700

    Add API from specification

--- CircularSuffixArray.java
+++ CircularSuffixArray.java
@@ -20,5 +20,8 @@
  * @author Michael <GrubenM@GMail.com>
  */
 public class CircularSuffixArray {
-    
+    public CircularSuffixArray(String s)    // circular suffix array of s
+    public int length()                     // length of s
+    public int index(int i)                 // returns index of ith sorted suffix
+    public static void main(String[] args)  // unit testing (required)
 }
--- MoveToFront.java
+++ MoveToFront.java
@@ -20,5 +20,14 @@
  * @author Michael <GrubenM@GMail.com>
  */
 public class MoveToFront {
-    
+    // apply move-to-front encoding, reading from standard input and writing to standard output
+    public static void encode()
+
+    // apply move-to-front decoding, reading from standard input and writing to standard output
+    public static void decode()
+
+    // if args[0] is '-', apply move-to-front encoding
+    // if args[0] is '+', apply move-to-front decoding
+    public static void main(String[] args)
+
 }
